554124b5d93718b4b9ae03aa7cb177df85c0509b
julesekmek <64405421+julesekmek@users.noreply.github.com>
2021-11-22 00:09:10 +0100

    2nd api

--- frontend/src/Content/Actu.js
+++ frontend/src/Content/Actu.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Actu.css';
 import Nav from './BandeauHaut';
 import Match from './Match';
+import axios from 'axios';
 
 import {Row} from 'react-bootstrap';
 import {Container} from 'react-bootstrap';
@@ -9,39 +10,120 @@ import {Col} from 'react-bootstrap';
 import EquipeClassement from './EquipeClassement';
 
 export default class Actu extends React.Component {
+   
+    constructor(props){
+        super(props)
+        this.state={
+            match1_homeTeam:[],
+            match1_VisitorTeam:[],
+            match1:[],
+
+            match2_homeTeam:[],
+            match2_VisitorTeam:[],
+            match2:[],
+
+            match3_homeTeam:[],
+            match3_VisitorTeam:[],
+            match3:[],
+
+            match4_homeTeam:[],
+            match4_VisitorTeam:[],
+            match4:[],
+            
+                        }
+                  }
+
+    async componentDidMount() {
+        const url_match = " https://free-nba.p.rapidapi.com/games"
+
+        let result_match = null;
+
     
-    render() {
+        try{
+                result_match= await axios(url_match, {
+                params: {page: '1', per_page: '10', team_ids: '1', Seasons: '2021'},
+                headers: {
+                    'x-rapidapi-host': 'free-nba.p.rapidapi.com',
+                    'x-rapidapi-key': 'ee837f376cmsh63e8fab63b39a11p1ad1b7jsn439f7d54cd93'
+                        }
+                                           })
+    
+
+    
+        } catch(e) {
+                console.log(e)
+                    }
+                    
+                this.setState({match1_homeTeam: result_match.data.data[0].home_team})
+                this.setState({match1_VisitorTeam: result_match.data.data[0].visitor_team})
+                this.setState({match1: result_match.data.data[0]})
+
+                this.setState({match2_homeTeam: result_match.data.data[6].home_team})
+                this.setState({match2_VisitorTeam: result_match.data.data[6].visitor_team})
+                this.setState({match2: result_match.data.data[6]})
+
+                this.setState({match3_homeTeam: result_match.data.data[2].home_team})
+                this.setState({match3_VisitorTeam: result_match.data.data[2].visitor_team})
+                this.setState({match3: result_match.data.data[2]})
+
+                this.setState({match4_homeTeam: result_match.data.data[3].home_team})
+                this.setState({match4_VisitorTeam: result_match.data.data[3].visitor_team})
+                this.setState({match4: result_match.data.data[3]})
+
+
+                   }
+
+
+    
+render() {
+
+        const{match1_homeTeam} = this.state;
+        const{match1_VisitorTeam} = this.state;
+        const{match1} = this.state;
+
+        const{match2_homeTeam} = this.state;
+        const{match2_VisitorTeam} = this.state;
+        const{match2} = this.state;
+
+        const{match3_homeTeam} = this.state;
+        const{match3_VisitorTeam} = this.state;
+        const{match3} = this.state;
+
+        const{match4_homeTeam} = this.state;
+        const{match4_VisitorTeam} = this.state;
+        const{match4} = this.state;
+
         let matchs= [{
-            ville1 : 'Los Angeles',
-            equipe1 : 'Lakers',
-            score1 : 97,
-            ville2 : 'New York',
-            equipe2 : 'Knicks',
-            score2 : 112
+            ville1 : match1_homeTeam.city,
+            equipe1 : match1_homeTeam.name,
+            score1 : match1.home_team_score,
+            ville2 : match1_VisitorTeam.city,
+            equipe2 : match1_VisitorTeam.name,
+            score2 :  match1.visitor_team_score
         },
         {
-            ville1 : 'Miami',
-            equipe1 : 'Heat',
-            score1 : 125,
-            ville2 : 'Orlando',
-            equipe2 : 'Magic',
-            score2 : 117
+            ville1 : match2_homeTeam.city,
+            equipe1 : match2_homeTeam.name,
+            score1 : match2.home_team_score,
+            ville2 : match2_VisitorTeam.city,
+            equipe2 : match2_VisitorTeam.name,
+            score2 :  match2.visitor_team_score
         },
         {
-            ville1 : 'Brooklyn',
-            equipe1 : 'Net',
-            score1 : 108,
-            ville2 : 'Detroit',
-            equipe2 : 'Pistons',
-            score2 : 84
+            ville1 : match3_homeTeam.city,
+            equipe1 : match3_homeTeam.name,
+            score1 : match3.home_team_score,
+            ville2 : match3_VisitorTeam.city,
+            equipe2 : match3_VisitorTeam.name,
+            score2 :  match3.visitor_team_score
         },
         {
-            ville1 : 'Sacramento',
-            equipe1 : 'Kings',
-            score1 : 106,
-            ville2 : 'Phoenix',
-            equipe2 : 'Suns',
-            score2 : 108
+            ville1 : match4_homeTeam.city,
+            equipe1 : match4_homeTeam.name,
+            score1 : match4.home_team_score,
+            ville2 : match4_VisitorTeam.city,
+            equipe2 : match4_VisitorTeam.name,
+            score2 :  match4.visitor_team_score
         },
         ];
 
